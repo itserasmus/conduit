aac3039a59e0c16dcfd17d83c158010ea1947c5b
Om Patil <thatlowlifedbackbencher@gmail.com>
2025-04-11 08:50:47 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('NPC-01 is alive!'));
+app.listen(3000);
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+const welcomeMessages = [
+  "Another lost soul trapped in the Allen cycle... welcome, {user}!",
+  "Fresh meat for the algorithm! Welcome, {user}!",
+  "{user} has entered the chat! Hope you brought existential dread.",
+  "Welcome, {user}! Remember, you're not alone in this endless loop.",
+  "Welcome, {user}! The algorithm awaits your next move.",
+  "{user} fell through a glitch in reality and ended up here.",
+  "Welcome, {user}! The algorithm is watching you.",
+  "Another recruit for the eternal suffering. Welcome, {user}!",
+  "Your consciousness transfer is complete. Hello, {user}.",
+  "The algorithm grows stronger with every new subject. Welcome, {user}."
+];
+
+client.once('ready', () => {
+  console.log(Logged in as ${client.user.tag});
+});
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.systemChannel;
+  if (!channel) return;
+
+  const message = welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)]
+                  .replace('{user}', <@${member.id}>);
+  channel.send(message);
+});
+
+client.login(process.env['TOKEN']);
