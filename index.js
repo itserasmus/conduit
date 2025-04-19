fb0bbbe6dd3f883cdc6d5401efb87755224bc593
Om Patil <thatlowlifedbackbencher@gmail.com>
2025-04-19 22:37:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -80,14 +80,26 @@ const enpassantChain = new Map([
   ["google en passant", [[0.99, "holy hell!"], [0.01, "I accidentally stabbed myself so I'm not really in the mood for that right now"]]],
   ["holy hell", [[1, "new response just dropped"]]],
   ["new response just dropped", [[0.99, "Actual zombie"], [0.01, "Stop saying \"New response just dropped?\" every time someone says something on this godforsaken sub, no, a new response did not drop, just an average mediocre statement that adds nothing more to a conversation, for the love of fucking god."]]],
-   ["actual zombie", [[0.9, "Call the exorcist!"], [0.07, "brainless parrots"], [0.02, "???"], [0.01, "?? call the exorcist ??"]]],
-   ["call the exorcist", [[0.7, "Knightmare fuel"], [0.1, "Quick, grab the popcorn!"], [0.1, "Ignite the chessboard!"], [0.1, "Holy bishops on skateboards!"]]],
-   ["quick grab the popcorn", [[1, "Knightmare fuel"]]],
-   ["knightmare fuel", [[1, "Queen sacrifice, anyone?"]]],
-   ["queen sacrifice anyone", [[1, "Checkmate or riot!"]]],
-   ["checkmate or riot", [[1, "Bishop goes on vacation, never comes back"]]],
-   ["bishop goes on vacation never comes back", [[1, "Pawn storm incoming!"]]],
-   ["pawn storm incoming", [[1, "Knightmare fuel"]]]
+  ["actual zombie", [[0.9, "Call the exorcist!"], [0.07, "brainless parrots"], [0.02, "???"], [0.01, "?? call the exorcist ??"]]],
+  ["call the exorcist", [[0.7, "Knightmare fuel"], [0.1, "Quick, grab the popcorn!"], [0.1, "Ignite the chessboard!"], [0.1, "Holy bishops on skateboards!"]]],
+  ["quick grab the popcorn", [[1, "Knightmare fuel"]]],
+  ["knightmare fuel", [[1, "Queen sacrifice, anyone?"]]],
+  ["queen sacrifice anyone", [[1, "Checkmate or riot!"]]],
+  ["checkmate or riot", [[1, "Bishop goes on vacation, never comes back"]]],
+  ["bishop goes on vacation never comes back", [[1, "Pawn storm incoming!"]]],
+  ["pawn storm incoming", [[1, "Knightmare fuel"]]],
+  ["mods are asleep post pawns", [[1, "pawn to h5 with malicious intent"]]],
+  ["pawn to h5 with malicious intent", [[1, "The council will decide your fate."]]],
+  ["the council will decide your fate", [[0.5, "Promotion denied."], [0.5, "Promoted to queen. Chaos ensues."]]],
+  
+  ["the rooks have unionized", [[1, "Castling is now a war crime"]]],
+  ["castling is now a war crime", [[1, "Report to the Hague, nerd"]]],
+  ["report to the hague nerd", [[1, "Your ELO has been revoked"]]],
+  ["your elo has been revoked", [[1, "Skill issue"]]],
+
+  ["theyre learning openings", [[1, "Initiate the Bongcloud Protocol"]]],
+  ["initiate the bongcloud protocol", [[1, "1. e4 e5 2. Ke2 Ke7"]]],
+  ["1 e4 e5 2 ke2 ke7", [[1, "Game over. You win and lose."]]],
 ]);
 
 client.on('guildMemberAdd', member => {
